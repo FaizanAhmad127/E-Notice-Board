fbcc9647f454e8705c53ebed91f5cef69aeb0743
Faizan <faizan@Faizan-MacBook-Pro.local>
2022-03-05 03:08:30 +0500

    drawer added

--- lib/ui/screens/student/student_root_screen/student_root_screen.dart
+++ lib/ui/screens/student/student_root_screen/student_root_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:notice_board/core/constants/colors.dart';
+import 'package:notice_board/core/constants/text_styles.dart';
 import 'package:notice_board/ui/screens/student/student_Project_Status_Screen/student_project_status_screen.dart';
 import 'package:notice_board/ui/screens/student/student_home_screen/student_home_screen.dart';
 import 'package:notice_board/ui/screens/student/student_notification_screen/student_notification_screen.dart';
@@ -27,13 +28,80 @@ SingleTickerProviderStateMixin{
     return Scaffold(
       resizeToAvoidBottomInset: false,
       drawer: Drawer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 2,
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: SizedBox(
+                  width: 1.sw,
+                  child: FittedBox(child:
+            Image(
+                  image: NetworkImage("https://blog.hubspot.com/hubfs/how-to-be-a-thought-leader-on-linkedin.jpg"),
+            ),),
+                ),
+              )),
+          Expanded(child: Padding(
+            padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "University ID",
+                    style: kPoppinsMedium500.copyWith(
+                      fontSize: 20.sp
+                    ),
+                  ),
+                  Row(
 
+                    children: [
+                      Icon(Icons.account_circle_rounded),
+                      SizedBox(width: 10.w,),
+                      Text("23864398",
+                        style: kPoppinsRegular400.copyWith(
+                            fontSize: 16.sp
+                        ),),
+                    ],
+                  )
+                ],
+              ))),
+          Expanded(child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Email Address",
+                    style: kPoppinsMedium500.copyWith(
+                        fontSize: 20.sp
+                    ),
+                  ),
+                  Row(
+
+                    children: [
+                      Icon(Icons.email_outlined),
+                      SizedBox(width: 10.w,),
+                      Text("Justinehr@gmail.com",
+                        style: kPoppinsRegular400.copyWith(
+                            fontSize: 16.sp
+                        ),),
+                    ],
+                  )
+                ],
+              ))),
+
+          Expanded(flex:1,child: Container())
+        ],
+      ),
       ),
       appBar: AppBar(
         backgroundColor: kPrimaryColor,
-        leading: Icon(
-          Icons.view_headline_sharp
-        ),
+        // leading: Icon(
+        //   Icons.view_headline_sharp
+        // ),
 
         actions: [
           Icon(
